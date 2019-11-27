087d36995ecf8b34a9af6c0501ba86c80eab4dc9
Emil Jaregran <emil@jaregran.se>
2019-11-27 14:38:58 +0100

    Implemented sys commands (except gpio)

--- RN2483-tester/main.py
+++ RN2483-tester/main.py
@@ -12,10 +12,20 @@ def main():
     print(f"Connected to {lora.serial_open()}\n")
     print(lora.get_version())
 
+    print("Module sleeping for 2000 ms...")
+    print(lora.sleep(2000))
+
+    # User-available EEPROM starts at address 768 and ends at 1023.
+    #print(lora.eeprom_save(768, 2))
+    #print(lora.eeprom_read(768))
+
+    print(f"VDD (mV): {lora.get_voltage()}")
+    print(f"Hardware EUI: {lora.get_hweui()}")
+
     lora.serial_close()
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main()
-    else:
+    if len(sys.argv) != 2:
         sys.exit("Usage: main.py <port>")
+
+    main()
--- RN2483-tester/rn2483.py
+++ RN2483-tester/rn2483.py
@@ -1,6 +1,5 @@
 import serial
 import sys
-from serial import SerialException
 
 class Lora:
     """Library for RN2483 modules from Microchip."""
@@ -17,8 +16,8 @@ class Lora:
         '''Start the serial communication.'''
         try:
             self.ser.open()
-        except SerialException:
-            sys.exit("Failed to open serial port, exiting...")
+        except serial.SerialException:
+            sys.exit(f"Failed to open port {self.ser.port}, exiting...")
 
         return self.ser.name
 
@@ -28,11 +27,39 @@ class Lora:
 
     def send_cmd(self, cmd):
         '''Send command to module and return the response.'''
-        self.ser.write(bytes(cmd + "\r\n", "utf-8"))
+        self.ser.write(bytes(f"{cmd}\r\n", "utf-8"))
         response = self.ser.readline().decode("utf-8")
 
         return response
 
+    def eeprom_read(self, address):
+        '''Read a byte(HEX) from the modules EEPROM at given address(HEX)'''
+        return self.send_cmd(f"sys get nvm {address:02x}")
+
+    def eeprom_save(self, address, byte):
+        '''Save a byte(HEX) in the modules EEPROM at given address(HEX).'''
+        return self.send_cmd(f"sys set nvm {address:02x} {byte:02x}")
+
+    def sleep(self, length):
+        '''Puts the module to sleep for the specified amount of milliseconds.'''
+        return self.send_cmd(f"sys sleep {length}")
+
+    def reset(self):
+        '''Reset and restart module.'''
+        return self.send_cmd("sys reset")
+
+    def factory_reset(self):
+        '''Reset module configuration to factory default values.'''
+        return self.send_cmd("sys factoryRESET")
+
     def get_version(self):
-        '''Get firmware version and release date.'''
+        '''Get module firmware version and release date.'''
         return self.send_cmd("sys get ver")
+
+    def get_voltage(self):
+        '''Get module VDD voltage.'''
+        return self.send_cmd("sys get vdd")
+
+    def get_hweui(self):
+        '''Get module Hardware EUI.'''
+        return self.send_cmd("sys get hweui")
