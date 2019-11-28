2ab6b2b5c7400fc979b27491fb66781153992683
Naima Claesson <naimaclaesson91@gmail.com>
2019-11-28 13:27:53 +0100

    Added TinyGPS+ lib

--- GPS_test/src/main.cpp
+++ GPS_test/src/main.cpp
@@ -1,24 +1,97 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
-SoftwareSerial mySerial(3, 5); // RX, TX
+static const int RXPin = 3, TXPin = 5;
+static const uint32_t GPSBaud = 9600;
 
-void setup(){
+TinyGPSPlus gps;
 
-  Serial.begin(9600);
-  while (!Serial) {
-    ; 
+SoftwareSerial ss(RXPin, TXPin);
+
+void displayInfo() {
+
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid()) {
+
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  } else {
+
+    Serial.print(F("INVALID"));
   }
 
-  mySerial.begin(9600);
-  mySerial.println("Hello, world?");
+  Serial.print(F("  Date/Time: "));
+
+  if (gps.date.isValid()) {
+
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  } else {
+
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+
+  if (gps.time.isValid()) {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+
+    if (gps.time.second() < 10) Serial.print(F("0"));
+
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+
+    Serial.print(gps.time.centisecond());
+  } else {
+
+    Serial.print(F("INVALID"));
+
+  }
+
+  Serial.println();
 }
 
-void loop(){
-  
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-    
-  if (Serial.available())
-    mySerial.write(Serial.read());
+void setup() {
+
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("\nSTARTING\n"));
+  Serial.println(F(" "));
+
 }
+
+void loop() {
+
+  while (ss.available() > 0){
+
+    if (gps.encode(ss.read())) {
+
+        displayInfo();
+        delay(1000);
+    }  
+
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+
+  }
+ }
+}
+
