0ae71f4770e7ed45aaef9e5f2aee133f8f267f4e
Naima Claesson <naimaclaesson91@gmail.com>
2019-12-03 10:24:00 +0100

    Adding code for LCD and TTN Mapper project

---
+++ LCD_test/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal.h> 
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int buttonPin = 8;
+int buttonState = 0;
+
+ void checkButton(int buttonState) {
+
+  if (buttonState == LOW) { 
+    return;
+
+    } else if (buttonState == HIGH){
+
+      lcd.print ("BUTTON PRESSED!");
+      Serial.println("BUTTON WAS PRESSED!");
+    
+    }  
+  }
+
+void setup() {
+
+  pinMode(buttonPin, INPUT_PULLUP); 
+  lcd.begin(16,2); 
+}
+
+void loop() {
+
+  lcd.clear();
+  buttonState = digitalRead(buttonPin);
+  checkButton(buttonState); 
+
+}
+
